1eefb48f4f468c8146606eda4ce471828e1386c1
FloraStvimaria <flostvimaria@gmail.com>
2024-09-03 10:02:03 +0800

    Create Day 2
    Method main dan sout
    Dan +

---
+++ Day2.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Perkenalkan saya" + 
+		                   " Flora Stvimaria");
+		System.out.println("Kelas Informatika D" + 
+		                   " Angkatan 2024");
+	}
+}
