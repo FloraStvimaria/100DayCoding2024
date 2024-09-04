d35d3aa7a39f6c701150e9a551e55e0ec5a7a9d4
FloraStvimaria <flostvimaria@gmail.com>
2024-09-04 13:52:06 +0800

    Day 3
    Aku buatnya ulang² kak😞

---
+++ Day3.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		int umur = 18;
+		String nama = "Flora Stvimaria";
+		double tinggibadan = 1.67;
+
+		System.out.println("Nama saya adalah " + 		nama +". Umur saya " + umur + "tahun.");
+		System.out.println("Tinggi badan saya " + 		tinggibadan + " meter.");
+	}
+}
