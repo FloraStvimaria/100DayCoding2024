d46ff080f7c97ce43879197137e65ae480ce50b8
FloraStvimaria <flostvimaria@gmail.com>
2024-09-05 20:05:02 +0800

    Day 4

---
+++ Day4.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+	    System.out.print("100 Days Coding ");
+        String nama = "Flora Stvimaria";
+        int umur = 18;
+        double tinggi = 1.67;
+        System.out.printf("Nama : %s Umur : %d Tinggi : %.2f sentimeter%n", nama, umur, tinggi);
+    }
+}
