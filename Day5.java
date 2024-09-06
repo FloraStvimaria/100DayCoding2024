898f755098514d970dccb3a2706e602872f5364f
FloraStvimaria <flostvimaria@gmail.com>
2024-09-06 11:28:36 +0800

    Day 5
    Menambahkan komentar

---
+++ Day5.java
@@ -0,0 +1,18 @@
+/*
+	* 100 Days Coding 2024
+	* For me : Yang rajin belajarnya
+*/
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Nama :" + 
+		                   " Flora Stvimaria");
+		System.out.println("Umur :" + 
+		                  " 18 Tahun");
+		System.out.println("Kelas : " + 
+		                   "Informatika D");
+		System.out.print("Alamat :" + 
+		                 " Aspuri 2 Pasangkayu");                 
+
+	}
+}
