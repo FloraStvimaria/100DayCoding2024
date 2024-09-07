cb2e817106a99688b60ba1c1a842802b76a5466b
FloraStvimaria <flostvimaria@gmail.com>
2024-09-07 10:07:56 +0800

    Day 6
    Variabel Deklarasi

---
+++ Day6.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		int umur =  //  Deklarasi tipe int
+		String nama =  // Deklarasi tipe String
+		double tinggi = // Deklarasi tipe double
+	}
+}
+
+Contoh
+ public class Main {
+	    public static void main(String[] args) {
+	    System.out.print("100 Days Coding ");
+        String nama = "Flora Stvimaria";
+        int umur = 18;
+        double tinggi = 1.67;
+        System.out.printf("Nama : %s Umur : %d Tinggi : %.2f sentimeter%n", nama, umur, tinggi);
+    }
+} 
