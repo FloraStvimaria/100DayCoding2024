deb1ee3b9ce07b8a64315bee317fe771cee879b3
FloraStvimaria <flostvimaria@gmail.com>
2024-09-09 19:03:53 +0800

    Day 8
    Menampilkan Variabel Update

---
+++ Day8.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		String Update = "Perkenalkan saya Flora Stvimaria";
+		System.out.println(Update);
+		
+		Update = "Kelas Informatika D " + 
+		                "Angkatan 24";
+		System.out.println(Update);
+
+	}
+}
