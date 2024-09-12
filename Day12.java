cbadaf088266cd7752f1bf113ec385c875c909d0
FloraStvimaria <flostvimaria@gmail.com>
2024-09-13 07:15:49 +0800

    Tipe Data : int

---
+++ Day12.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		int panjang = 100;
+		int lebar = 20;
+		int cucu = 4;
+		int tanah = panjang*lebar;
+		System.out.println(tanah/cucu);
+	}
+}
