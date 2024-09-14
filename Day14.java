28c27c9c01ee064ec67ca635a616ab39916920fb
FloraStvimaria <flostvimaria@gmail.com>
2024-09-15 07:56:48 +0800

    Tipe Data : long

---
+++ Day14.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+			// deklarasi variabel angin dengan tipe data long
+		long angin = 135;
+		
+			// nilai variabel angin
+		System.out.println("Nilai long : " + angin);
+	}
+}
