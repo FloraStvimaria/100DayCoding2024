6e2574bda5fc80cfb568afa9512984a9a8e23ce2
FloraStvimaria <flostvimaria@gmail.com>
2024-09-17 18:45:26 +0800

    Tipe Data : float

---
+++ Day16.java
@@ -0,0 +1,8 @@
+public class Day15 {
+    
+    public static void main(String[] args) {
+        float panjangkayu =14f;
+        System.out.println("Panjang Kayu : " + panjangkayu + "cm");
+    }
+    
+}
