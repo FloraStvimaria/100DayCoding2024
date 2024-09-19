c50a361883c07cc86f2e0fc02bfad3647683b120
FloraStvimaria <flostvimaria@gmail.com>
2024-09-19 18:07:05 +0800

    Tipe Data : boolean
    Menampilkan hasil true dan false

---
+++ Day18.java
@@ -0,0 +1,12 @@
+public class Day18 {
+	public static void main(String[] args) {
+		int angka1 = 50;
+		int angka2 = 100;
+		
+		boolean hasil1 = angka1 < angka2;
+		boolean hasil2 = angka2 < angka1;
+		
+		System.out.println("Hasilnya adalah : " + hasil1);
+		System.out.println("Hasilnya adalah : " + hasil2);
+	}
+}
