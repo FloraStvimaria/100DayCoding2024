ce6abe97111b14cd544adae84ea4b478b50a6c19
FloraStvimaria <flostvimaria@gmail.com>
2024-09-20 18:57:59 +0800

    Tipe Data Reference : String

--- Day19.java
+++ Day19.java
@@ -2,7 +2,7 @@ public class Day19 {
 	public static void main(String[] args) {
 		String namadepan = "Uzumaki";
 		String namabelakang = "Naruto";
-		String namalengkap = ("Nama Lengkap : " + 		namadepan + " " + namabelakang);
+		String namalengkap = ("Nama Lengkap : " + namadepan + " " + namabelakang);
 		System.out.println(namalengkap);
 	}
 }
