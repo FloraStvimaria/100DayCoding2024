cf1148e3103b7d30cb50aaac4840f9e23e9269a1
FloraStvimaria <flostvimaria@gmail.com>
2024-09-25 19:57:51 +0800

    Tipe Data Reference : Float

---
+++ Day24.java
@@ -0,0 +1,7 @@
+public class Day24 {
+    public static void main(String[] args) {
+        Float panjangpapan = 14.0f;
+        System.out.println("Panjang Papan : " + panjangpapan + "cm");
+    }
+    
+}
