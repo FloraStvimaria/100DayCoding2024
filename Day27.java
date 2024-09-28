1393d231f330c14cc6a34fa3784d2879193c22b7
FloraStvimaria <flostvimaria@gmail.com>
2024-09-28 21:39:53 +0800

    Evaluasi Besar1

---
+++ Day27.java
@@ -0,0 +1,15 @@
+package D0224034;
+
+public class FloraStvimaria {
+    
+    public static void main(String[] args) {
+        
+        //System.out.println("Hello, World!");
+        
+        String flora;
+        flora = "Hello World";
+        
+        System.out.println("Flora : " + flora);
+    }
+    
+}
