8e0d4c2394c9a575180b69b1390458196a8a22d2
FloraStvimaria <flostvimaria@gmail.com>
2024-09-29 18:31:09 +0800

    Tipe Data Reference : Boolean

---
+++ Day28.java
@@ -0,0 +1,9 @@
+public class Day28 {
+	public static void main(String[] args) {
+	Boolean kopi = false;
+    Boolean gula = true;
+    
+    System.out.println("Kopi asin : " + kopi);
+    System.out.println("Gula manis : " + gula);
+	}
+}
