51f67fce95fbd4d1e4189be9a3426996a59aa1d8
FloraStvimaria <flostvimaria@gmail.com>
2024-09-30 19:26:17 +0800

    Tipe Data Reference : BigInteger

---
+++ Day29.java
@@ -0,0 +1,9 @@
+import java.math.BigInteger;
+public class Day29 {
+    
+    public static void main(String[] args) {
+        BigInteger a = new BigInteger ("271000000000000");
+        System.out.println("Uang saya : " + a);
+    }
+    
+}
