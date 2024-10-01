6e10fd8c78f0f6993291331998db8ede1f2775c5
FloraStvimaria <flostvimaria@gmail.com>
2024-10-01 20:11:12 +0800

    Tipe Data Reference : BigDecimal

---
+++ Day30.java
@@ -0,0 +1,9 @@
+import java.math.BigDecimal;
+public class Day30 {
+    
+    public static void main(String[] args) {
+        BigDecimal a = new BigDecimal ("230.000");
+        System.out.println("Duit : " + a);
+    }
+    
+}
