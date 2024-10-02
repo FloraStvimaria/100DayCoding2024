fd9ed8b5f26c59047659bfa94de8f5f6f180a635
FloraStvimaria <flostvimaria@gmail.com>
2024-10-02 20:58:48 +0800

    Konversi String ke Tipe Data Primitif

---
+++ Day31.java
@@ -0,0 +1,9 @@
+public class Day31 {
+    
+    public static void main(String[] args) {
+        String angka = "150";
+        int angka1 = Integer.parseInt(angka);
+        System.out.println("Nilai : " + angka1);
+    }
+    
+}
