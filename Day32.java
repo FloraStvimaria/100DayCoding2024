259fbb3ce298786f2b4f617ff8a609cba76df019
FloraStvimaria <flostvimaria@gmail.com>
2024-10-03 20:51:48 +0800

    Konversi Data Primitif ke String

---
+++ Day32.java
@@ -0,0 +1,9 @@
+public class Day32 {
+    
+    public static void main(String[] args) {
+        int angka1 = 123;
+        String angka2 = String.valueOf(angka1);
+        System.out.println("Hasil : " + angka2);
+    }
+    
+}
