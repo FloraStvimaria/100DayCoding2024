3c0b332f4d7fb6749f4840dd71f3404de6ce6aea
FloraStvimaria <flostvimaria@gmail.com>
2024-10-04 21:18:23 +0800

    Type Casting

---
+++ Day33.java
@@ -0,0 +1,9 @@
+public class Day33 {
+    
+    public static void main(String[] args) {
+        double a = 50.5;
+        int b = (int) a;
+        System.out.println("Hasil : " + b);
+    }
+    
+}
