640918846826df4164b302aef7e24263f2c32dc5
FloraStvimaria <flostvimaria@gmail.com>
2024-10-06 23:03:25 +0800

    Operator Aritmatika : Penjumlahan & Pengurangan

---
+++ Day35.java
@@ -0,0 +1,12 @@
+public class Day35 {
+    public static void main(String[] args) {
+        int a,b,c,d;
+        a = 20;
+        b = 10;
+        c = a+b;
+        d = a-b;
+        System.out.println("Hasil : " + c);
+        System.out.println("Hasil : " + d);
+    }
+    
+}
