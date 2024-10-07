9f8e7a4c94aa456e5e034936c60801a3f9aa909d
FloraStvimaria <flostvimaria@gmail.com>
2024-10-07 20:04:46 +0800

    Operator Aritmatika : Perkalian, Pembagian serta Modulo

---
+++ Day36.java
@@ -0,0 +1,12 @@
+
+public class Day36 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 65;
+        b = 30;
+        System.out.println("Hasil dari * : " + a*b);
+        System.out.println("Hasil dari / : " + a/b);
+        System.out.println("Hasil dari % : " + a%b);
+    }
+    
+}
