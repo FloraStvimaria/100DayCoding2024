41912b346031b78c2822dd5c4cdf4babca1ca3a5
FloraStvimaria <flostvimaria@gmail.com>
2024-10-08 18:46:51 +0800

    Operator Penugasan : Pengisian Nilai, Pengisian dan  Penambahan, Pengisian dan Pengurangan

---
+++ Day37.java
@@ -0,0 +1,11 @@
+public class Day37 {
+    public static void main(String[] args) {
+        int a = 15;
+        System.out.println("Hasil dari  = : " + a);
+        a += 5;
+        System.out.println("Hasil dari += : " + a);
+        a -= 2;
+        System.out.println("Hasil dari -= : " + a);
+    }
+    
+}
