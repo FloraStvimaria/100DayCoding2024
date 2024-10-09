c2e7a3229e21d1b626c04613c035011a4e7dc762
FloraStvimaria <flostvimaria@gmail.com>
2024-10-09 19:01:45 +0800

    Operator Penugasan : Pengisian dan Perkalian, Pengisian dan  Pembagian

---
+++ Day38.java
@@ -0,0 +1,10 @@
+public class Day38 {
+    public static void main(String[] args) {
+        int a = 15;
+        a *= 2;
+        System.out.println("Hasil Dari *= : " + a);
+        a /= 2;
+        System.out.println("Hasil Dari /= : " + a);
+    }
+    
+}
