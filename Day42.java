127e482ae0031c07b08c863761be9013428dd860
FloraStvimaria <flostvimaria@gmail.com>
2024-10-13 18:41:27 +0800

    Operator Perbandingan : Sama dengan dan tidak sama dengan

---
+++ Day42.java
@@ -0,0 +1,11 @@
+public class Day42 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 10;
+        b = 10;
+        
+        System.out.println(a == b );
+        System.out.println( a != b);
+    }
+    
+}
