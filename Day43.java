c3d8841f3d6bba49af1b40fece7d3c4c23de75d9
FloraStvimaria <flostvimaria@gmail.com>
2024-10-14 19:37:04 +0800

    Operator Perbandingan : Lebih besar sama dengan dan lebih  kecil sama dengan

---
+++ Day43.java
@@ -0,0 +1,10 @@
+public class Day43 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 20;
+        b = 20;
+        System.out.println(a >= 3);
+        System.out.println(b <= 3);
+    }
+    
+}
