4965ee0016f6a01e6a310c5842d989697bf8b20c
FloraStvimaria <flostvimaria@gmail.com>
2024-10-15 18:57:38 +0800

    Operator Logika : AND

---
+++ Day44.java
@@ -0,0 +1,11 @@
+public class Day44 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 10;
+        b = 5;
+        
+        System.out.println(a>=b && b<=a);
+        System.out.println(a<=b && b>=a);
+    }
+    
+}
