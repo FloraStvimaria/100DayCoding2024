80e3a80a5a261ccc070ba2b3a7a702b584865f1f
FloraStvimaria <flostvimaria@gmail.com>
2024-10-16 19:56:43 +0800

    Operator Logika : OR

---
+++ Day45.java
@@ -0,0 +1,12 @@
+
+public class Day45 {
+    public static void main(String[] args) {
+        int f,s;
+        f = 50;
+        s = 25;
+        
+        System.out.println(f==s && s==f);
+        System.out.println(f/=s && s/=f);
+     
+    }
+}
