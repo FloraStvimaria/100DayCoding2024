5cf2b136796ede2e5b2062dcf2ecb4bdcce7b5de
FloraStvimaria <flostvimaria@gmail.com>
2024-10-17 18:53:35 +0800

    Operator Logika : Negasi/kebalikan

---
+++ Day46.java
@@ -0,0 +1,11 @@
+public class Day46 {
+    public static void main(String[] args){
+        boolean gula,gulaa;
+        gula = true;
+        gulaa = !gula;
+        
+        System.out.println("Apakah gula mainis: " + gula);
+        System.out.println("Apakah gulaa tidak manis : " + gulaa);
+        
+    }
+}
