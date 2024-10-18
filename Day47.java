557411bc760c57dafa6c9b76447de929231a4031
FloraStvimaria <flostvimaria@gmail.com>
2024-10-18 18:54:54 +0800

    Percabangan : if

---
+++ Day47.java
@@ -0,0 +1,10 @@
+public class Day47 {
+    public static void main(String[] args) {
+        boolean kopi = false;
+        
+        if (!kopi) {
+          System.out.println("Kopi pahit");  
+        }
+    }
+    
+}
