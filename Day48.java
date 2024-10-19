88d17d9573c51950299106e5006ab8e80ed19037
FloraStvimaria <flostvimaria@gmail.com>
2024-10-19 19:23:00 +0800

    Percabangan : if - else

---
+++ Day48.java
@@ -0,0 +1,15 @@
+public class Day48 {
+    public static void main(String[] args) {
+       int hari = 48;
+       
+       System.out.print("Hari ini waktunya : ");
+       if (hari >= 40) {
+         System.out.println("coding");
+       }else {
+         System.out.println("turu");
+       }
+        
+        
+    }
+    
+}
