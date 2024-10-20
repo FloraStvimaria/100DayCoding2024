eea9192316e3de22b71edab326d24bbe82af8692
FloraStvimaria <flostvimaria@gmail.com>
2024-10-20 16:26:55 +0800

    Percabangan : if - else if

---
+++ Day49.java
@@ -0,0 +1,14 @@
+public class Day49 {
+    public static void main(String[] args) {
+       int s = 50;
+        
+        System.out.print("Hei kamu : ");
+        if (s == 20 ){
+            System.out.println("yeah");
+        }else if (s >= 40){
+            System.out.println("yessir");
+        }
+       
+    }
+    
+}
