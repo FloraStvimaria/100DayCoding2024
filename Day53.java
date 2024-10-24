20119ca55ff3c23127c3117c7c60b736ba5bcb28
FloraStvimaria <flostvimaria@gmail.com>
2024-10-24 09:53:20 +0800

    Looping : for

---
+++ Day53.java
@@ -0,0 +1,10 @@
+public class Day53 {
+    public static void main(String[] args) {
+        
+        for (int flo = 15; flo >= 10; flo--) {
+           System.out.println("Nilai : " + flo); 
+        } 
+        
+    }
+    
+}
