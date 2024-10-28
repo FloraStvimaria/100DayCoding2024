19fece37931cf3542b915d79065af68c0596b542
FloraStvimaria <flostvimaria@gmail.com>
2024-10-28 19:04:17 +0800

    Looping : while Increment

---
+++ Day57.java
@@ -0,0 +1,9 @@
+public class Day57 {
+    public static void main(String[] args) {
+        int f = 10;
+        while (f <= 20){
+            System.out.print(f + " ");
+            f++;
+        }
+    }
+}
