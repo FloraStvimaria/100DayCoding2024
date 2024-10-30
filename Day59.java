8cf1268ee08e5d94d15a5af1b13e58155e696854
FloraStvimaria <flostvimaria@gmail.com>
2024-10-30 19:03:23 +0800

    Looping : For Each

---
+++ Day59.java
@@ -0,0 +1,9 @@
+public class Day59 {
+    public static void main(String[] args) {
+        String[] hari = {"Senin", "Selasa", "Rabu"};
+        
+        for (String harii : hari){
+            System.out.print(harii + " ");
+        }
+    }
+}
