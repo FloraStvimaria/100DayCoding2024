846c7193a472c2e18f5e93a28e4253116524211f
FloraStvimaria <flostvimaria@gmail.com>
2024-10-31 19:26:33 +0800

    Looping : do while

---
+++ Day60.java
@@ -0,0 +1,10 @@
+public class Day60 {
+    public static void main(String[] args) {
+       int flo = 10;
+        
+        do {
+            System.out.print(flo + " ");
+            flo--;
+        }while (flo >= 1);
+    }
+}
