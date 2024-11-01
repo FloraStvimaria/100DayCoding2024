0a7a6cffc34d319e079a008873a8460fe4721967
FloraStvimaria <flostvimaria@gmail.com>
2024-11-01 18:01:28 +0800

    Infinite Loop

---
+++ Day61.java
@@ -0,0 +1,9 @@
+public class Day61 {
+    public static void main(String[] args) {
+        int flo = 0;
+        
+        do {
+            System.out.println("Arigatou gozimasu");
+        }while (flo <= 0);
+    }
+}
