acf90a06157fabf2af2826b8b39bd33fe12b9d2c
FloraStvimaria <flostvimaria@gmail.com>
2024-11-05 20:33:27 +0800

    Nested Loop

---
+++ Day65.java
@@ -0,0 +1,12 @@
+public class Day65 {
+    public static void main(String[] args) {
+        int f = 3; 
+
+        for (int s = 1; s <= f; s++) {  
+            for (int l = 1; l <= f; l++) {  
+                System.out.print(" Yessir!!! ");
+            }
+            System.out.println();  
+        }
+    }
+}
