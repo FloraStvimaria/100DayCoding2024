2b1d6bdbda6a36691512c40234ea5f51f13bbe2c
FloraStvimaria <flostvimaria@gmail.com>
2024-11-07 19:26:45 +0800

    Method Void

---
+++ Day.java
@@ -0,0 +1,13 @@
+public class Day68 {
+    public static void main(String[] args) {
+        flo();
+    }
+    public static void flo() {
+        for (int f = 0; f < 10; f++){
+            if (f == 4){
+               break; 
+            } 
+            System.out.print(f + " ");
+        }
+    }
+}
