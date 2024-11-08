72d9934e13d6dea2e87a9929386c114b03f47e79
FloraStvimaria <flostvimaria@gmail.com>
2024-11-08 19:48:12 +0800

    Method Non-void

---
+++ Day69.java
@@ -0,0 +1,10 @@
+public class Day69 {
+    public static int luas(int p, int l) {
+        return p * l; 
+    }
+
+    public static void main(String[] args) {
+        int hasil = luas(10, 5);
+        System.out.println("Hasil: " + hasil);
+    }
+}
