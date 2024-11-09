8b57d4eb5149daf9158daa15c20954841ad863eb
FloraStvimaria <flostvimaria@gmail.com>
2024-11-09 15:24:13 +0800

    Keyword : break

---
+++ Day62.java
@@ -0,0 +1,10 @@
+public class Day62 {
+    public static void main(String[] args) {
+        for (int f = 0; f < 10; f++){
+            if (f == 4){
+               break; 
+            } 
+            System.out.print(f + " ");
+        }
+    }
+}
