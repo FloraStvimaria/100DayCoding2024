09d1b7db75571d9bfa62f9f9659a4bae764dbc74
FloraStvimaria <flostvimaria@gmail.com>
2024-11-09 15:12:50 +0800

    Evaluasi Mentor 3

---
+++ Day69.java
@@ -0,0 +1,13 @@
+public class EvaluasiMentor3 {
+    public static void main(String[] args) {
+        int f = 6;
+        int s,y;
+        for (s = f; s >= 1; s--) {
+            for (y = 1; y <= s; y++) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
