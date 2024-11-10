d6529c30dee4564a39215f232b0a856a9bf6b0dc
FloraStvimaria <flostvimaria@gmail.com>
2024-11-10 20:50:18 +0800

    Method Parameter

---
+++ Dah70.java
@@ -0,0 +1,12 @@
+public class Day70 {
+    public int luas(int p, int l) {
+        return p * l;
+    }
+    
+    public static void main(String[] args) {
+        Day70 flo = new Day70();
+        int luas = flo.luas(10,5);
+        System.out.println("Hasil : " + luas);
+    }
+    
+}
