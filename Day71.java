88cc3adbc67dbff1919fb46997075d14b2026b0f
FloraStvimaria <flostvimaria@gmail.com>
2024-11-11 23:06:40 +0800

    Method Static

---
+++ Day71.java
@@ -0,0 +1,10 @@
+public class Day71 {
+    public static int luas(int p, int l) {
+        return p * l;
+    }
+    public static void main(String[] args) {
+        int luas = luas(20,10);
+        System.out.println("Hasil : " + luas);
+    }
+    
+}
