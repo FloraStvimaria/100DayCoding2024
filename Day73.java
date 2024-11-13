82b47b751f84224f1c29284b2b084cdf916a486d
FloraStvimaria <flostvimaria@gmail.com>
2024-11-13 22:45:07 +0800

    String Method : charAt()

---
+++ Day73.java
@@ -0,0 +1,8 @@
+public class Day73 {
+    public static void main(String[] args) {
+        String prodi = "Informatika";
+        System.out.println(prodi);
+        System.out.println("prodi gweh : " + prodi.charAt(0));
+        
+    }
+}
