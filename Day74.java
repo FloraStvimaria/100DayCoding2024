7a9e00d5f4db01f665306ab347c0a0861d8c4a57
FloraStvimaria <flostvimaria@gmail.com>
2024-11-14 18:34:31 +0800

    String Method : length()

---
+++ Day74.java
@@ -0,0 +1,9 @@
+public class Day74 {
+    public static void main(String[] args) {
+        String n = "Flora Stvimaria";
+        int p = n.length();
+        System.out.println("Nama : " + n);
+        System.out.println("Jumlah : " + p);
+    }
+    
+}
