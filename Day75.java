ab4cbef9d3aad82e5f5e451689a3344e175932d8
FloraStvimaria <flostvimaria@gmail.com>
2024-11-15 23:07:43 +0800

    String Method : toLowerCase()

---
+++ Day75.java
@@ -0,0 +1,10 @@
+public class Day75 {
+    public static void main(String[] args) {
+        String b,k;
+        b = "FLORA";
+        k = b.toLowerCase();
+        System.out.println("Huruf Besar : " + b);
+        System.out.println("Huruf Kecil : " + k);
+    }
+    
+}
