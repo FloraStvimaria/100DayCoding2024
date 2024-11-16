a7b73cfa1a3bf31b917acd6bb072976e4d1bb39a
FloraStvimaria <flostvimaria@gmail.com>
2024-11-16 19:28:47 +0800

    String Method : toUpperCase()

---
+++ Day76.java
@@ -0,0 +1,10 @@
+public class Day76 {
+    public static void main(String[] args) {
+        String k,b;
+        k = "flora";
+        b = k.toUpperCase();
+        System.out.println("Huruf Kecil : " + k);
+        System.out.println("Huruf Besar : " + b);
+    }
+    
+}
