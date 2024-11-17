f4cf02a5c6fa74445c863a470a1b5e9d5a3fa677
FloraStvimaria <flostvimaria@gmail.com>
2024-11-17 18:29:16 +0800

    String Method : equals()

---
+++ Day77.java
@@ -0,0 +1,11 @@
+public class Day77 {
+    public static void main(String[] args) {
+        String f,l,o;
+        f = "Saya";
+        l = "Kamu";
+        o = "Saya";
+        System.out.println("Hasil : " + f.equals(l));
+        System.out.println("Hasil : " + f.equals(o));
+    }
+    
+}
