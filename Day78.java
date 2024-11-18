6bdab72dbf895918fc91792fcd9ffb8946b558f3
FloraStvimaria <flostvimaria@gmail.com>
2024-11-18 17:45:57 +0800

    String Method : equalsIgnoreCase()

---
+++ Day78.java
@@ -0,0 +1,11 @@
+public class Day78 {
+    public static void main(String[] args) {
+        String f,l,o;
+        f = "Saya";
+        l = "Kamu";
+        o = "SAYA";
+        System.out.println("Hasil : " + f.equalsIgnoreCase(l));
+        System.out.println("Hasil : " + f.equalsIgnoreCase(o));
+    }
+    
+}
