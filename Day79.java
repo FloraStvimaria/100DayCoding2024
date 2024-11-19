c1b0eeb1507696be957dc5342719b3a8c2b5c1ab
FloraStvimaria <flostvimaria@gmail.com>
2024-11-19 19:20:28 +0800

    String Method : toString()

---
+++ Day79.java
@@ -0,0 +1,8 @@
+public class Day79 {
+    public static void main(String[] args) {
+        int a = 15000;
+        String t = Integer.toString(a);
+        System.out.println("Hasil : " + t);
+    }
+    
+}
