096ec73282a5853af4a6eeaa734df22447910a3b
FloraStvimaria <flostvimaria@gmail.com>
2024-11-21 18:17:31 +0800

    String Method : valueOf()

---
+++ Day81.java
@@ -0,0 +1,8 @@
+public class Day81 {
+    public static void main(String[] args) {
+        char[] flo = {'F','L','O','R','A'};
+        System.out.println("Hasil : " + String.valueOf(flo));
+        
+    }
+    
+}
