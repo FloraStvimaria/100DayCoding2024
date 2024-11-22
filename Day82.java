25c204ab3f7819e0d087aa4f21910b4ff657d3d2
FloraStvimaria <flostvimaria@gmail.com>
2024-11-22 18:55:56 +0800

    Method ceil(), floor() dan round()

---
+++ Day82.java
@@ -0,0 +1,12 @@
+public class Day82 {
+    public static void main(String[] args) {
+        double c,f,r;
+        c = 4.8;
+        f = 4.5;
+        r = 2.8;
+        System.out.println(Math.ceil(c));
+        System.out.println(Math.floor(f));
+        System.out.println(Math.round(r));
+    }
+    
+}
