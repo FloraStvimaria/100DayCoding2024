c1e8b263d80c9c0125fbac278d21e616274c1d7e
FloraStvimaria <flostvimaria@gmail.com>
2024-11-23 18:41:19 +0800

    Membuat Objek Array satu dimensi

---
+++ Day83.java
@@ -0,0 +1,13 @@
+public class Day83 {
+    public static void main(String[] args) {
+        int[] flo = new int[3];
+        flo[0] = 10;
+        flo[1] = 15;
+        flo[2] = 20;
+        
+        System.out.println("Hasil : ");
+        for (int f = 0; f < flo.length; f++) {
+            System.out.println("flo[" + f + "] = " + flo[f]);
+        }
+    }
+}
