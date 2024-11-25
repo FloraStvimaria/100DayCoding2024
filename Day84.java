b778ea79ef6ee9f681b48ca36a75373c2151e174
FloraStvimaria <flostvimaria@gmail.com>
2024-11-25 20:42:22 +0800

    Mengisi elemen ke Array satu dimensi pada saat di inisialisasi

---
+++ Day84.java
@@ -0,0 +1,8 @@
+public class Day84 {
+    public static void main(String[] args) {
+        int[] flo = {1, 2, 3};
+        for (int f = 0; f < flo.length; f++) {
+            System.out.println("Flo[" + f + "] = " + flo[f]);
+        }
+    }
+}
