c34ed0d7bb18c2c14bb3cd863d9780c67b600f6d
FloraStvimaria <flostvimaria@gmail.com>
2024-11-26 19:39:47 +0800

    Mencetak elemen pada Array satu dimensi

---
+++ Day86.java
@@ -0,0 +1,9 @@
+public class Day86 {
+    public static void main(String[] args) {
+        int[] flo = {4, 5, 6, 7, 8};
+        for (int f = 0; f < flo.length; f++) {
+            System.out.println("Flo[" + f + "] = " + flo[f]);
+            
+        }
+    }
+}
