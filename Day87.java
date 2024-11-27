f5c4f15b2389dca0ba3279c0d2bc78caed2e54e0
FloraStvimaria <flostvimaria@gmail.com>
2024-11-27 19:23:36 +0800

    Mengubah Nilai Array satu dimensi pada index Tertentu

---
+++ Day87.java
@@ -0,0 +1,9 @@
+public class Day87 {
+    public static void main(String[] args) {
+        int[] flo = {14, 15, 16, 17, 18};
+        System.out.println("Sebelum diubah : " + flo[3]);
+        
+        flo[3] = 77;
+        System.out.println("Setelah diubah : " + flo[3]);
+    }
+}
