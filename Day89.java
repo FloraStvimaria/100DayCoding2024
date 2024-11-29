3f1dcc7d4536d813ee716d8a9c11e775d2d11b12
FloraStvimaria <flostvimaria@gmail.com>
2024-11-29 19:56:48 +0800

    Soal2

--- Day89.java
+++ Day89.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class tes89 {
     public static void main(String[] args) {
         Scanner flo = new Scanner(System.in);
-        boolean isPalindrome = false;
+        boolean f = false;
 
-        while (!isPalindrome) {
+        while (!f) {
             System.out.print("Masukkan kata : ");
             String ra = flo.nextLine().trim();
 
             String lo = new StringBuilder(ra).reverse().toString();
             if (ra.equals(lo)) {
                 System.out.println("Yes");
-                isPalindrome = true;  
+                f = true;  
             } else {
                 System.out.println("No, coba lagi!");
             }
